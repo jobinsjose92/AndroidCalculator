78803e67231a82d22138c1429c831b8a17438814
jobinsjose92 <50811203+jobinsjose92@users.noreply.github.com>
2021-11-02 08:42:38 -0500

    button

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,55 +15,66 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void Add(View v)
-    {
-        EditText t1=(EditText)findViewById(R.id.editTextNumber);
-        EditText t2=(EditText)findViewById(R.id.editTextNumber2);
-        EditText t3=(EditText)findViewById(R.id.editTextNumber3);
+    public void Add(View v) {
+        EditText t1 = (EditText) findViewById(R.id.editTextNumber);
+        EditText t2 = (EditText) findViewById(R.id.editTextNumber2);
+        EditText t3 = (EditText) findViewById(R.id.editTextNumber3);
 
         int n1 = Integer.parseInt(t1.getText().toString());
         int n2 = Integer.parseInt(t2.getText().toString());
-        int result = n1+n2;
+        int result = n1 + n2;
 
-        t3.setText("Total Value "+result);
+        t3.setText("" + result);
     }
 
-    public void Subtract(View v)
-    {
-        EditText t1=(EditText)findViewById(R.id.editTextNumber);
-        EditText t2=(EditText)findViewById(R.id.editTextNumber2);
-        EditText t3=(EditText)findViewById(R.id.editTextNumber3);
+    public void Subtract(View v) {
+        EditText t1 = (EditText) findViewById(R.id.editTextNumber);
+        EditText t2 = (EditText) findViewById(R.id.editTextNumber2);
+        EditText t3 = (EditText) findViewById(R.id.editTextNumber3);
 
         int n1 = Integer.parseInt(t1.getText().toString());
         int n2 = Integer.parseInt(t2.getText().toString());
-        int result = n1-n2;
+        int result = n1 - n2;
 
-        t3.setText("difference Value "+result);
+        t3.setText("" + result);
     }
-    public void Multiply(View v)
-    {
-        EditText t1=(EditText)findViewById(R.id.editTextNumber);
-        EditText t2=(EditText)findViewById(R.id.editTextNumber2);
-        EditText t3=(EditText)findViewById(R.id.editTextNumber3);
+
+    public void Multiply(View v) {
+        EditText t1 = (EditText) findViewById(R.id.editTextNumber);
+        EditText t2 = (EditText) findViewById(R.id.editTextNumber2);
+        EditText t3 = (EditText) findViewById(R.id.editTextNumber3);
 
         int n1 = Integer.parseInt(t1.getText().toString());
         int n2 = Integer.parseInt(t2.getText().toString());
-        int result = n1*n2;
+        int result = n1 * n2;
 
-        t3.setText("Product Value "+result);
+        t3.setText("" + result);
     }
 
-    public void Divide(View v)
-    {
-        EditText t1=(EditText)findViewById(R.id.editTextNumber);
-        EditText t2=(EditText)findViewById(R.id.editTextNumber2);
-        EditText t3=(EditText)findViewById(R.id.editTextNumber3);
+    public void Divide(View v) {
+        EditText t1 = (EditText) findViewById(R.id.editTextNumber);
+        EditText t2 = (EditText) findViewById(R.id.editTextNumber2);
+        EditText t3 = (EditText) findViewById(R.id.editTextNumber3);
 
         int n1 = Integer.parseInt(t1.getText().toString());
         int n2 = Integer.parseInt(t2.getText().toString());
-        int result = n1/n2;
+        int result = n1 / n2;
 
-        t3.setText("Quotient Value "+result);
+        t3.setText("" + result);
     }
+
+    public void hide(View v)
+    {
+    try
+
+    {
+        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+    } catch(Exception e)
+
+    {
+        // TODO: handle exception
+    }
+}
 
 }
